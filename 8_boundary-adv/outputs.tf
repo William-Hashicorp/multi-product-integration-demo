70791f33470e7e3b3d0c4067e7e4cb27d178aa73
William Yang <william.yang@hashicorp.com>
2024-04-09 09:44:55 +0800

    .

--- 8_boundary-adv/outputs.tf
+++ 8_boundary-adv/outputs.tf
@@ -2,3 +2,15 @@
 output "oidc_auth_method_id" {
   value = boundary_auth_method_oidc.oidc_auth.id
 }
+
+output "boundary_public_endpoint" {
+  value = data.terraform_remote_state.hcp_clusters.outputs.boundary_public_endpoint
+}
+
+output "vault_public_endpoint" {
+  value = data.terraform_remote_state.hcp_clusters.outputs.vault_public_endpoint
+}
+
+output "consul_public_endpoint" {
+  value = data.terraform_remote_state.hcp_clusters.outputs.consul_public_endpoint
+}
