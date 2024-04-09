1278130a5d36f51d422c64d67c55e964ae4fef9a
William Yang <william.yang@hashicorp.com>
2024-04-09 14:33:58 +0800

    .

--- 10_aad_management/variables.tf
+++ 10_aad_management/variables.tf
@@ -1,17 +1,4 @@
-variable "client_id" {
-  description = "The Client ID of the Azure Service Principal."
-  type        = string
-}
 
-variable "client_secret" {
-  description = "The Client Secret for the Service Principal."
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "The Tenant ID of the Azure AD."
-  type        = string
-}
 
 variable "user_account_name" {
   description = "The User Principal Name (UPN) of the Azure AD User."
